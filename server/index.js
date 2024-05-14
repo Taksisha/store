7d0418ccd09d320158643257333067e7c038bdaa
Tanya <dasharomanova836@gmail.com>
2024-05-14 13:52:38 +0300

    i

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
+app.get('/', (req, res) =>{
+    res.status(200).json({message: 'WORKING!!!'})
+})
 
 const start = async () => {
     try{
-        await sequelize.aunthenticate()
+        await sequelize.authenticate()
         await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
