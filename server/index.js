1bd5f48b3503fa9562589a9715af82f747f0a6f6
Tanya <dasharomanova836@gmail.com>
2024-05-14 16:40:34 +0300

    i

--- server/index.js
+++ server/index.js
@@ -4,14 +4,12 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
+const router = require('./routes/index')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-app.get('/', (req, res) =>{
-    res.status(200).json({message: 'WORKING!!!'})
-})
+app.use('./api', router)
 
 const start = async () => {
     try{
